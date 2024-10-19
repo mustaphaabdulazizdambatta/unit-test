7a0aaf8ba24e6ea0063eb1d3221d63c9fe2332af
mac <mac@macs-MacBook-Pro.local>
2024-10-19 13:27:31 -0700

    Save progress before installing Chainlink contracts

--- test/FundMeTest.t.sol
+++ test/FundMeTest.t.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: SEE LICENSE IN LICENSE
 pragma solidity ^0.8.28;
 
-import {Test} from "forge-std/Test";
-import {FundMe} from "../FundMe.sol";
+import "forge-std/Test.sol";
+import {FundMe} from "../src/FundMe.sol";
 
 contract FundTest is Test {
     FundMe fundMe;
