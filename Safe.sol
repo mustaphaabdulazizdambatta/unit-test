ed93ef9eada3cbe8e2842165a861aad88364b594
musty_code.py <120594595+mustaphaabdulazizdambatta@users.noreply.github.com>
2024-12-02 09:56:17 -0800

    Add files via upload

---
+++ Safe.sol
@@ -0,0 +1,12 @@
+pragma solidity 0.8.10;
+
+import {Test} from "forge-std/Test.sol";
+
+contract Safe {
+    receive() external payable {}
+
+    function withdraw() external {
+        payable(msg.sender).transfer(address(this).balance);
+    }
+}
+
